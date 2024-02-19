bbf0ff3ca82ad68496d2daeec67211430035ddc8
“venommer” <1553047996@qq.com>
2024-02-19 21:23:48 +0800

    local static

--- NewProject/NewProject/src/Main.cpp
+++ NewProject/NewProject/src/Main.cpp
@@ -1,49 +1,58 @@
 #include <iostream>
-#include <string>
-#include <vector>
 
-struct Vertex
+class Singleton1
 {
-	float x, y, z;
-
-	Vertex(float x,float y,float z)
-		:x(x),y(y),z(z)
+private:
+	static Singleton1* s_instance;
+public:
+	static Singleton1& Get()
 	{
-
+		return *s_instance;
 	}
 
-	Vertex(const Vertex& vertice)
-		:x(vertice.x), y(vertice.y), z(vertice.z)
-	{
-		std::cout << "Copied!" << std::endl;
-	}
+	void Hello() {}
 };
 
-std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
+Singleton1* Singleton1::s_instance = nullptr;
+
+
+
+class Singleton2
 {
-	stream << vertex.x << ", " << vertex.y << ", " << vertex.z;
-	return stream;
+public:
+	static Singleton2& Get()
+	{
+		static Singleton2 instance;//create singleton instance this way will make our code much cleaner.
+		return instance;
+	}
+
+	void Hello() {}
+};
+
+//lifetime decides how long the varible will exists in memory,scope decides where it can be accessed.
+void Function()
+{
+	int i = 0;//if we call this function,it will always print 1.
+	i++;
+	std::cout << i << std::endl;
 }
 
-/*
-* once the vector is resized,vector will copy all the data and reallocate them.
-* the push_back function construct objects in the main function memory,and copy it to the vector memory,
-* so we can optimize the vector in two ways,
-* 1. set enough capacity for vector so that its not going to resize
-* 2. make the vector construct objects in the memory allocated for vector straightly so that we can avoid copying.
-*/
+void Function2()
+{
+	static int i = 0;//makes lifetime of the varible lasts in the whole program,but the scope of the varible constrained in the function,can not be accessed outside.
+	i++;
+	std::cout << i << std::endl;
+}
+
+int i = 0;//we can change the lifetime this way,but the scope of the varible is global,can be accessed anywhere,and this is not we want.
+
 int main()
 {
-	std::vector<Vertex> vertices;
-	//this will construct 3 objects,and it wont through the compile,we should use reserve function just to set capacity and do not have to construct any objects
-	//std::vector<Vertex> vertices(3);
-	vertices.reserve(3);//set enough capacity
-
-	//we should use emplace_back function which means we tell the vector to construct objects in the vector memory straightly to avoid copying.
-	//vertices.push_back(Vertex(1,2,3));
-
-	vertices.emplace_back(1,2,3);//just pass the parameters we need to constrct objects,do not have to use intialize list.
-	vertices.emplace_back(4,5,6);
-	vertices.emplace_back(7,8,9);
+	/*Function();
+	Function();
+	Function();
+	Function();
+	Function();*/
+	Singleton2::Get().Hello();
 	std::cin.get();
 }
